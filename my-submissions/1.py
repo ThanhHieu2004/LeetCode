36ae45818d6827ae78270ebec464836f60cb8f7d
Thanh Hieu <thanhhieu8304@gmail.com>
2025-03-29 10:28:46 +0700

    Add 1 Ex

---
+++ my-submissions/1.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # for i in range(len(nums)):
+        #     next_num = target - nums[i]
+        #     if next_num in nums[i+1:]:
+        #         i_2 = nums.index(next_num, i+1,)
+        #         return [i, i_2]
+        # return []
+        nums_dict = {}
+        result = []
+        for i, num in enumerate(nums):
+            nums_dict[num] = i
+            subtracted_value = target-num
+            if subtracted_value in nums_dict:
+                if i != nums_dict[subtracted_value]:
+                    return [nums_dict[target-num], i]
+                elif i == nums_dict[subtracted_value]:
+                    result.append(i)
+                    continue
+        return result
+        
+        
+
+        
